41404c83317ad93c119c8173d799574b3d4a3a32
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-04-12 16:36:24 +0530

    learnt callback hell

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
-let DATA = "secret information";
+// callback hell//
 
-class user{
-    constructor(name, email){
-        this.name = name;
-        this.email = email;
-    }
-
-    viewData(){
-        console.log("data = ", DATA);
-    }
+function getData(dataId, genNextData){
+     setTimeout(() =>{
+         console.log("data", dataId);
+            if(genNextData){
+                genNextData();
+            }
+        }, 2000);
 }
 
-class admin extends user{
-    constructor(name, email){
-        super(name, email);
-        
-    }
-    editData(){
-        DATA = "some new value";
-    }
-}
-
-let student1 = new user("Om", "omdarade1399@gmail.com");
-let student2 = new user("Pooja", "poojadarade1504@gmail.com"); 
-
-let teacher1 = new user("Dean", "dean123@email.com");
-
-let admin1 = new admin("admin", "admin123@email.com");
+getData(1, () => {
+    getData(2, () => {
+        getData(3, () => {
+            getData(4, () => {
+                getData(5);
+            });
+        });
+    });
+}); 
