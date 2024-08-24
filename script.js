d01d557d68f085f611b87c1a1840eb4c050f0353
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-08-24 14:10:33 +0530

    Appended Array Data in a table  using DOM and forEach loop

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+let tableBody = document.querySelector('tbody');
+
+let Students = [
+    ['Om' , 9637272740 , 'Male' , 25],
+    ['Pooja' , 8788005680 , 'Female' , 20],
+    ['Sanjay' , 9890167863 , 'Male' , 50],
+    ['Shubhangi' , 9923328506 , 'Female' , 48]
+];
 
 
 
+for(let i=1; i<Students.length; i++){}
+
+Students.forEach((student, i) => {
+    let row = document.createElement('tr');
+    let srCell = document.createElement('td');
+    srCell.textContent = i + 1;
+    row.appendChild(srCell);
+    student.forEach(data => {
+        let col = document.createElement('td');
+        col.textContent = data;
+        row.appendChild(col);
+    }); 
+    tableBody.appendChild(row);
+});
